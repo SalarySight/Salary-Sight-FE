5fbcc76599d8a9f210c0a15c031edde56376917f
darlaevans2000 <darlaevans2000@gmail.com>
2021-10-18 11:51:52 -0600

    Add grid to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
 import logo from './logo.png';
+import "./Header.css";
 import { Link } from "react-router-dom";
 
 
 const Header = () => {
   return (
     <header className="header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="logo" alt="logo" />
       <h1>Salary Sight</h1>
+      <button>Login</button>
     </header>
   );
 };
