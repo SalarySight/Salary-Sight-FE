885218d8b3e09aefd6fb8529aa5505d77280be1d
darlaevans2000 <darlaevans2000@gmail.com>
2021-10-18 15:15:34 -0600

    Add form basics

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,28 +1,34 @@
+import React from 'react'
 import Header from "../Header/Header";
 import Cover from "../Cover/Cover";
+import SlideDrawer from '../Form/SlideDrawer.js'
+import Backdrop from '../Form/Backdrop.js'
+import MainPage from '../Form/MainPage.js'
 import { Route, Switch } from "react-router-dom";
 import './App.css';
 
-function App() {
+class App extends React.Component {
+state = { drawerOpen: false }
+   drawerToggleClickHandler = () => {
+       this.setState({
+         drawerOpen: !this.state.drawerOpen
+       })
+   }
+   render(){
+      let backdrop;
+      if(this.state.drawerOpen){
+        backdrop = <Backdrop/>;
+       }
+
   return (
     <div className="App">
       <Header />
       <Cover />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < SlideDrawer show={this.state.drawerOpen}/>
+             { backdrop }
+      < MainPage toggle={this.drawerToggleClickHandler}/>
     </div>
   );
 }
-
-export default App;
+}
+export default App
---
+++ src/Components/Form/SlideDrawer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './SlideDrawer.css'
+import Form from './Form'
+
+export default class SlideDrawer extends React.Component {
+   render(){
+       let drawerClasses = 'side-drawer'
+       if(this.props.show) {
+          drawerClasses = 'side-drawer open'
+       }
+       return(
+   
+          <div className={drawerClasses}>
+             <Form>    
+             </Form>
+          </div>
+)
+    }
+    
+}
