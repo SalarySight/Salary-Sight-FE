15b5572f96caa631a0209ee6b95ba062f04a2978
darlaevans2000 <darlaevans2000@gmail.com>
2021-10-21 16:18:19 -0600

    Add queries for fetching data and refactor APICalls into utils

---
+++ src/Components/utils/getCards.js
@@ -0,0 +1,9 @@
+import { fetchData } from './utils';
+import * as gql from '../queries/queries';
+
+
+export const getCards = () => {
+      const cardQuery = gql.getPosts();
+      const cardResult = fetchData(cardQuery);
+  return cardResult
+};
