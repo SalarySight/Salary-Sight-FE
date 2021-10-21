f532bc2d387570cb5b5cd8637081e2efaecbb40f
darlaevans2000 <darlaevans2000@gmail.com>
2021-10-21 17:17:50 -0600

    Get basic form object working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,54 +1,55 @@
-import React, { Component } from 'react'
-import Header from '../Header/Header';
-import Cover from '../Cover/Cover';
-import SlideDrawer from '../Form/SlideDrawer'
-import Backdrop from '../Form/Backdrop'
-import MainPage from '../Form/MainPage'
-import { getCards } from '../utils/getCards'
-import './App.css';
+import React, { Component } from "react";
+import Header from "../Header/Header";
+import Cover from "../Cover/Cover";
+import SlideDrawer from "../Form/SlideDrawer";
+import Backdrop from "../Form/Backdrop";
+import MainPage from "../Form/MainPage";
+import { getCards } from "../utils/getCards";
+import "./App.css";
 
 class App extends Component {
-   state = {
-     drawerOpen: false,
-     posts: [],
-     error: ""
-   }
+  state = {
+    drawerOpen: false,
+    posts: [],
+    error: "",
+  };
 
   componentDidMount = () => {
-    this.addCards()
-  }
+    this.addCards();
+  };
 
   addCards = () => {
-    getCards().then(data => {
-      this.setState({ posts: data.posts })
-    })
-  }
-  
+    getCards().then((data) => {
+      this.setState({ posts: data.posts });
+    });
+  };
 
-drawerToggleClickHandler = () => {
+  drawerToggleClickHandler = () => {
     this.setState({
-      drawerOpen: !this.state.drawerOpen
-    })
-  }
-backdropClickHandler = () => {
+      drawerOpen: !this.state.drawerOpen,
+    });
+  };
+  
+  backdropClickHandler = () => {
     this.setState({
-      drawerOpen: false
-    })
-  }
-   render(){
-      let backdrop;
-      if(this.state.drawerOpen){
-        backdrop = <Backdrop close={this.backdropClickHandler}/>;
-       }
-      return(
-         <div>
-          <Header />
-          <Cover />
-           <SlideDrawer show={this.state.drawerOpen} />
-           { backdrop }
-           <MainPage toggle={this.drawerToggleClickHandler} />
-         </div>
-      )
+      drawerOpen: false,
+    });
+  };
+
+  render() {
+    let backdrop;
+    if (this.state.drawerOpen) {
+      backdrop = <Backdrop close={this.backdropClickHandler} />;
     }
+    return (
+      <div>
+        <Header />
+        <Cover />
+        <SlideDrawer show={this.state.drawerOpen} />
+        {backdrop}
+        <MainPage toggle={this.drawerToggleClickHandler} />
+      </div>
+    );
+  }
 }
-export default App
+export default App;
--- src/Components/utils/utils.js
+++ src/Components/utils/utils.js
@@ -15,3 +15,21 @@ export const fetchData = async body => {
 
   return result.data;
 };
+
+export const postData = async body => {
+  const url = 'https://salary-sight-be.herokuapp.com/graphql';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  });
+
+  const result = await response;
+  if (result.errors) {
+    throw new Error(result.errors[0].message);
+  }
+
+  return result
+};
