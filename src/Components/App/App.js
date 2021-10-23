8a1e77b8d5c1492d8858987a0e03f300ba62aeb2
Erica <ericaspitz522@gmail.com>
2021-10-23 15:34:17 -0600

    Add filtering for age and age groups and error handling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,7 @@ import Backdrop from "../Form/Backdrop";
 import MainPage from "../Form/MainPage";
 import SalaryCards from "../SalaryCards/SalaryCards";
 import Loader from "../Loader/Loader";
-import Error from "../Error/Error";
+import NoMatchError from "../NoMatchError/NoMatchError";
 import NotFound from '../NotFound/NotFound';
 import FilterForm from '../FilterForm/FilterForm';
 import { useQuery } from '@apollo/client';
@@ -17,12 +17,11 @@ import "./App.css";
 const App = () => {
   const [drawerOpen, setDrawerOpen] = useState(false)
   const [salaryPosts, setSalaryPosts] = useState([])
+  const [filterPosts, setFilterPosts] = useState([])
   const [filterInput, setFilterInput] = useState('')
+  const [filterError, setFilterError] = useState('')
+  const [ageInput, setAgeInput] = useState('')
   const { data, loading, error } = useQuery(GET_POST)
-
-useEffect(() => {
-  setSalaryPosts(data)
-}, [data])
 
 const drawerToggleClickHandler = () => {
   setDrawerOpen(!drawerOpen)
@@ -32,10 +31,57 @@ const backdropClickHandler = () => {
   setDrawerOpen(false)
 };
 
-const handleInput = (e) => {
+const handleFilterInput = (e) => {
   console.log(e.target.value)
   setFilterInput(e.target.value)
 }
+
+const handleAgeInput = (e) => {
+  setAgeInput(e.target.value)
+}
+
+useEffect(() => {
+  if (loading) {
+    console.log('loading', loading)
+  } else if (error) {
+    console.log('error', error)
+  } else {
+    setSalaryPosts(data)
+    console.log('salaryPOSTS', salaryPosts)
+    console.log('data', data)
+  }
+  if (data) console.log(data.posts)
+}, [data])
+
+const filterData = (filterInput, input) => {
+  setFilterPosts([])
+  setFilterError('')
+  const prop = filterInput.toLowerCase()
+  const matchCards = salaryPosts.posts.filter(post => post[prop] === input)
+  console.log('matched', matchCards)
+  console.log('before', salaryPosts)
+  if (matchCards.length !== 0) {
+    setFilterPosts(matchCards)
+  } else if (matchCards.length === 0) {
+    setFilterError('No matches!')
+  } else {
+    return null;
+  }
+}
+
+// const cleanData = () => {
+//   if (data) {
+//     return data
+//   } else if (data && salaryPosts.length) {
+//     return salaryPosts
+//   } else {
+//     console.log('EFFED UP')
+//   }
+// }
+
+useEffect(() => {
+  console.log('salaryPosts', salaryPosts)
+}, [salaryPosts])
 
   return (
     <div>
@@ -44,9 +90,16 @@ const handleInput = (e) => {
       <SlideDrawer toggle={drawerToggleClickHandler} show={drawerOpen} />
       {drawerOpen && <Backdrop close={backdropClickHandler} />}
       <MainPage toggle={drawerToggleClickHandler} />
-      <FilterForm value={filterInput} handleInput={handleInput}/>
+      <FilterForm
+        filterInput={filterInput}
+        handleFilterInput={handleFilterInput}
+        ageInput={ageInput}
+        handleAgeInput={handleAgeInput}
+        filterData={filterData}/>
       {loading && <Loader />}
-      {!loading && !error && <SalaryCards data={data.posts}/>}
+      {!loading && !error && filterPosts.length === 0 && !filterError && <SalaryCards data={data.posts}/>}
+      {!loading && !error && filterPosts.length !== 0 && <SalaryCards data={filterPosts}/>}
+      {!loading && filterError && filterPosts.length === 0 && <NoMatchError />}
     </div>
   );
 }
---
+++ src/Components/NoMatchError/NoMatchError.js
@@ -0,0 +1,11 @@
+import './NoMatchError.css';
+
+const NoMatchError = () => {
+  return (
+    <div className='no-match-error'>
+      <h3>No results</h3>
+    </div>
+  );
+}
+
+export default Error;
