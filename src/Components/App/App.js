354d46e80a6361dfe2d88e12dd255282fb8d67b1
Erica <ericaspitz522@gmail.com>
2021-10-23 10:27:53 -0600

    Mes with button type submit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -17,8 +17,6 @@ const App = () => {
   const { data, loading, error } = useQuery(GET_POST)
 
 useEffect(() => {
-  console.log('error', error)
-  console.log('loading', loading)
   setSalaryPosts(data)
 }, [data])
 
@@ -43,59 +41,4 @@ const backdropClickHandler = () => {
   );
 }
 
-
-// {!salaryPosts.length && !error && <Loader />}
-// {error && !salaryPosts.length && <Error error={error} />}
-
-
-// import { getCards } from "../utils/getCards";
-
-// class App extends Component {
-//   state = {
-//     drawerOpen: false,
-//     posts: [],
-//     error: ""
-//   };
-//
-//   componentDidMount = () => {
-//     this.addCards();
-//   };
-//
-//   addCards = () => {
-//     getCards()
-//     .then((data) => this.setState({ posts: data.posts }))
-//     .catch(err => this.setState({ error: err }))
-//   };
-//
-  // drawerToggleClickHandler = () => {
-  //   this.setState({
-  //     drawerOpen: !this.state.drawerOpen,
-  //   });
-  // };
-//
-  // backdropClickHandler = () => {
-  //   this.setState({
-  //     drawerOpen: false,
-  //   });
-  // };
-//
-//   render() {
-    // let backdrop;
-    // if (this.state.drawerOpen) {
-    //   backdrop = <Backdrop close={this.backdropClickHandler} />;
-    // }
-//     return (
-    //   <div>
-    //     <Header />
-    //     <Cover />
-    //     <SlideDrawer show={this.state.drawerOpen} addCards={this.addCards} />
-    //     {backdrop}
-    //     <MainPage toggle={this.drawerToggleClickHandler} />
-    //     {this.state.error && !this.state.posts.length && <Error error={this.state.error} />}
-    //     {!this.state.posts.length && !this.state.error && <Loader />}
-    //     <SalaryCards data={this.state.posts}/>
-    //   </div>
-    // );
-//   }
-// }
 export default App;
