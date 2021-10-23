0a6de479c9ec6a584272bb4faffefaa8d3f58159
Erica <ericaspitz522@gmail.com>
2021-10-23 12:21:13 -0600

    Change all components to functional components and close drawer on submit

--- src/Components/Form/MainPage.js
+++ src/Components/Form/MainPage.js
@@ -1,11 +1,11 @@
-import React from 'react'
-export default class MainPage extends React.Component {
-   render(){
-     return (
-        <div className="login-submit-btns">
-          <button className="submit-btn" onClick={this.props.toggle}>Submit Salary</button>
-          <button className="login-btn"><a href="https://github.com/login/oauth/authorize?client_id=03061f342b90340a4ce6">Login</a></button>
-        </div>
-     )
-    }
+
+const MainPage = ({ toggle }) => {
+   return (
+      <div className="login-submit-btns">
+        <button className="submit-btn" onClick={toggle}>Submit Salary</button>
+        <button className="login-btn"><a href="https://github.com/login/oauth/authorize?client_id=03061f342b90340a4ce6">Login</a></button>
+      </div>
+   );
 }
+
+export default MainPage;
