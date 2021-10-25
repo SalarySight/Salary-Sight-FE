6f9c5ceb493b5cdd0146ae3d6a8cafd7e5fa6c37
Alex Klick <aklick12@gmail.com>
2021-10-25 12:16:24 -0600

    updated filter form

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { Route, Switch } from 'react-router-dom'
+import { useState, useEffect, useRef } from "react";
+import { Route, Switch } from "react-router-dom";
 import Header from "../Header/Header";
 import Cover from "../Cover/Cover";
 import SlideDrawer from "../Form/SlideDrawer";
@@ -8,66 +8,94 @@ import MainPage from "../Form/MainPage";
 import SalaryCards from "../SalaryCards/SalaryCards";
 import Loader from "../Loader/Loader";
 import NoMatchError from "../NoMatchError/NoMatchError";
-import NotFound from '../NotFound/NotFound';
-import FilterForm from '../FilterForm/FilterForm';
-import { useQuery } from '@apollo/client';
-import { GET_POST } from '../..';
+import NotFound from "../NotFound/NotFound";
+import FilterForm from "../FilterForm/FilterForm";
+import { useQuery } from "@apollo/client";
+import { GET_POST } from "../..";
 import "./App.css";
 
 const App = () => {
-  const [drawerOpen, setDrawerOpen] = useState(false)
-  const [salaryPosts, setSalaryPosts] = useState([])
-  const [filterPosts, setFilterPosts] = useState([])
-  const [filterInput, setFilterInput] = useState('')
-  const [filterError, setFilterError] = useState('')
-  const [input, setInput] = useState('')
-  const { data, loading, error } = useQuery(GET_POST)
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [salaryPosts, setSalaryPosts] = useState([]);
+  const [filterPosts, setFilterPosts] = useState([]);
+  const [filterInput, setFilterInput] = useState("");
+  const [filterError, setFilterError] = useState("");
+  const [filterState, setFilterState] = useState({});
+  const [toggle, setToggle] = useState(true);
+  const [input, setInput] = useState([]);
+  const { data, loading, error } = useQuery(GET_POST);
 
-const drawerToggleClickHandler = () => {
-  setDrawerOpen(!drawerOpen)
-};
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+  const drawerToggleClickHandler = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
-const backdropClickHandler = () => {
-  setDrawerOpen(false)
-};
+  const backdropClickHandler = () => {
+    setDrawerOpen(false);
+  };
 
-const handleFilterInput = (e) => {
-  setFilterInput(e.target.value)
-}
+  const handleFilterInput = (e) => {
+    setFilterInput(e.target.value);
+  };
 
-const handleInput = (e) => {
-  setInput(e.target.value)
-}
+  const handleInput = (e) => {
+    setInput(e.target.value);
+    console.log(input);
+  };
+  const handleFilterChange = (key, newValue) => {
+    const currentValues = [];
+    handleToggle();
+    key = key.toLowerCase();
+    if (newValue === "") {
+      let update = filterState;
+      delete update[key];
+      setFilterState(update);
+    } else {
+      setFilterState({ ...filterState, [key]: [...currentValues, newValue] });
+    }
+  };
+  const clearFilterForm = (e) => {
+    console.log("hello");
+    setFilterInput("");
+    setFilterPosts([]);
+  };
 
-const clearFilterForm = (e) => {
-  console.log('hello')
-  setFilterInput('')
-  setFilterPosts([])
-}
+  useEffect(() => {
+    if (loading) {
+      console.log("loading", loading);
+    } else if (error) {
+      console.log("error", error);
+    } else {
+      setSalaryPosts(data);
+    }
+  }, [data]);
 
-useEffect(() => {
-  if (loading) {
-    console.log('loading', loading)
-  } else if (error) {
-    console.log('error', error)
-  } else {
-    setSalaryPosts(data)
-  }
-}, [data])
+  const didMount = useRef(true);
 
-const filterData = (filterInput, input) => {
-  setFilterPosts([])
-  setFilterError('')
-  const prop = filterInput.toLowerCase()
-  const matchCards = salaryPosts.posts.filter(post => post[prop] === input)
-  if (matchCards.length !== 0) {
-    setFilterPosts(matchCards)
-  } else if (matchCards.length === 0) {
-    setFilterError('No matches!')
-  } else {
-    return null;
-  }
-}
+  useEffect(() => {
+    if (didMount.current) {
+      didMount.current = false;
+    } else {
+      filterData();
+    }
+  }, [filterState, toggle]);
+
+  const filterData = () => {
+    var filteredCodes = salaryPosts.posts;
+    Object.keys(filterState).map(function (key, index) {
+      function getFilteredCodes(array, key, value) {
+        return array.filter(function (e) {
+          return e[key] == value;
+        });
+      }
+
+      filteredCodes = getFilteredCodes(filteredCodes, key, filterState[key]);
+    });
+
+    filteredCodes !== [] ? setFilterPosts(filteredCodes) : setFilterPosts([]);
+  };
 
   return (
     <div>
@@ -76,19 +104,26 @@ const filterData = (filterInput, input) => {
       <SlideDrawer toggle={drawerToggleClickHandler} show={drawerOpen} />
       {drawerOpen && <Backdrop close={backdropClickHandler} />}
       <MainPage toggle={drawerToggleClickHandler} />
+
       <FilterForm
         filterInput={filterInput}
         handleFilterInput={handleFilterInput}
         input={input}
         handleInput={handleInput}
         filterData={filterData}
-        clearFilterForm={clearFilterForm}/>
-      {loading && <Loader />}
-      {!loading && !error && filterPosts.length === 0 && !filterError && <SalaryCards data={data.posts}/>}
-      {!loading && !error && filterPosts.length !== 0 && <SalaryCards data={filterPosts}/>}
+        clearFilterForm={clearFilterForm}
+        handleFilterChange={handleFilterChange}
+        salaryPosts={salaryPosts}
+      />
+      {!loading && !error && filterPosts.length === 0 && !filterError && (
+        <SalaryCards data={data.posts} />
+      )}
+      {!loading && !error && filterPosts.length !== 0 && (
+        <SalaryCards data={filterPosts} />
+      )}
       {!loading && filterError && filterPosts.length === 0 && <NoMatchError />}
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/FilterForm/FilterForm.js
+++ src/Components/FilterForm/FilterForm.js
@@ -1,112 +1,73 @@
-import './FilterForm.css'
-import { useState }  from 'react'
-
-const FilterForm = ({ filterInput, handleFilterInput, input, handleInput, filterData, clearFilterForm }) => {
-  const [addFilterOpt, setAddFilterOpt] = useState(false)
-
-  const addFilter =() => {
-    setAddFilterOpt(true)
+import "./FilterForm.css";
+import { useState, useEffect, useRef } from "react";
+import { Dropdown, Menu, Grid } from "semantic-ui-react";
+const FilterForm = ({ handleFilterChange, salaryPosts }) => {
+  const [addFilterOpt, setAddFilterOpt] = useState(false);
+  const [companyOptions, setCompanyOptions] = useState([]);
+  const companyRef = useRef(0);
+  const ageOptions = [
+    { key: "18-24", text: "18-24", value: "18-24" },
+    { key: "25-34", text: "25-34", value: "25-34" },
+    { key: "35-44", text: "35-44", value: "35-44" },
+  ];
+  const genderOptions = [
+    { key: "Woman", text: "Woman", value: "Woman" },
+    { key: "Man", text: "Man", value: "Man" },
+    { key: "Transgender", text: "Transgender", value: "Transgender" },
+  ];
+  function getCompanyOptions() {
+    if (salaryPosts.posts) {
+      let options = salaryPosts.posts.map(function (post) {
+        return { key: post.company, text: post.company, value: post.company };
+      });
+      setCompanyOptions(options);
+    }
   }
-
+  useEffect(() => {
+    companyRef.current++;
+    if (companyRef.current < 4) {
+      getCompanyOptions();
+    }
+  });
   return (
     <>
-    <select
-      className='filter'
-      name='filter'
-      type='search'
-      value={filterInput}
-      onChange={(e) => handleFilterInput(e)}>
-
-      <option value="" disabled selected>
-        Filter By...
-      </option>
-      <option value="Age">Age</option>
-      <option value="Gender">Gender</option>
-      {/*<option value="Salary">Salary</option>*/}
-      {/*add more filter options*/}
-    </select>
-
-    {filterInput === 'Age' &&
-    <select
-      className='filter-age'
-      name='age'
-      type='search'
-      value={input}
-      onChange={(e) => handleInput(e)}>
-
-      <option value="" disabled selected>
-        Age Options
-      </option>
-      <option value="18-24">18-24</option>
-      <option value="25-34">25-34</option>
-      <option value="35-44">35-44</option>
-      <option value="45-54">45-54</option>
-      <option value="55-64">55-64</option>
-      <option value="65+">65+</option>
-    </select>}
-
-      {filterInput === 'Gender' &&
-      <select
-        className='filter-gender'
-        name='gender'
-        type='search'
-        value={input}
-        onChange={(e) => handleInput(e)}>
-
-        <option value="" disabled selected>
-          Gender Options
-        </option>
-        <option value="Woman">Woman</option>
-        <option value="Man">Man</option>
-        <option value="Transgender">Transgender</option>
-        <option value="Non-binary/non-conforming">
-          Non-binary/non-conforming
-        </option>
-        <option value="Prefer not to respond">Prefer not to respond</option>
-      </select>}
-      {/*{filterInput === 'Salary' &&
-      <select
-        className='filter-salary'
-        name='salary'
-        type='search'
-        value={input}
-        onChange={(e) => handleInput(e)}>
-        <option value="" disabled selected>
-          Salary Ranges
-        </option>
-        <option value="<60k">Less than 60k</option>
-        <option value="60-80k">60k-80k</option>
-        <option value="80-100k">80k-100k</option>
-        <option value="100k-120k">100-120k</option>
-        <option value="120k+">120k+</option>
-      </select>}*/}
-      {addFilterOpt && <select
-        className='filter'
-        name='filter'
-        type='search'
-        value={filterInput}
-        onChange={(e) => handleFilterInput(e)}>
-
-        <option value="" disabled selected>
-          Filter By...
-        </option>
-        <option value="Age">Age</option>
-        <option value="Gender">Gender</option>
-        <option value="Salary">Salary</option>
-      </select>}
-      {!filterInput &&
-      <select
-        className='empty'
-        name='empty'
-        type='search'>
-        <option value="" disabled selected>
-        </option></select>}
-      <button onClick={(e)=> filterData(filterInput, input)}>Search</button>
-      {/*<button onClick={(e) => addFilter(e)}>Add Filter</button>*/}
-      <button onClick={(e) => clearFilterForm(e)}>Remove Filters</button>
-
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={6}>
+            <Menu secondary>
+              <Dropdown
+                placeholder="Age"
+                fluid
+                search
+                clearable
+                selection
+                options={ageOptions}
+                onChange={(el, e) => handleFilterChange(e.placeholder, e.value)}
+              />
+              <Dropdown
+                placeholder="Gender"
+                fluid
+                search
+                clearable
+                selection
+                options={genderOptions}
+                onChange={(el, e) => handleFilterChange(e.placeholder, e.value)}
+              />
+              <Dropdown
+                placeholder="Company"
+                fluid
+                search
+                clearable
+                selection
+                options={companyOptions}
+                onChange={(el, e) => handleFilterChange(e.placeholder, e.value)}
+              />
+            </Menu>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </>
   );
-}
+};
 
-export default FilterForm
+export default FilterForm;
