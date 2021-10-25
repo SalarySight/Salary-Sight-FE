3bfe773a485bc754efb41c6f727b8b4c0fd72928
Alex Klick <aklick12@gmail.com>
2021-10-25 17:31:36 -0600

    added other filter

--- src/Components/FilterForm/FilterForm.js
+++ src/Components/FilterForm/FilterForm.js
@@ -5,30 +5,47 @@ const FilterForm = ({ handleFilterChange, salaryPosts }) => {
   const [addFilterOpt, setAddFilterOpt] = useState(false);
   const [companyOptions, setCompanyOptions] = useState([]);
   const companyRef = useRef(0);
+
   const ageOptions = [
     { key: "18-24", text: "18-24", value: "18-24" },
     { key: "25-34", text: "25-34", value: "25-34" },
     { key: "35-44", text: "35-44", value: "35-44" },
   ];
+
   const genderOptions = [
     { key: "Woman", text: "Woman", value: "Woman" },
     { key: "Man", text: "Man", value: "Man" },
     { key: "Transgender", text: "Transgender", value: "Transgender" },
   ];
+
   function getCompanyOptions() {
     if (salaryPosts.posts) {
-      let options = salaryPosts.posts.map(function (post) {
+      const options = salaryPosts.posts.map(function (post) {
         return { key: post.company, text: post.company, value: post.company };
       });
-      setCompanyOptions(options);
+      const distinctOptions = [];
+      const map = new Map();
+      for (const item of options) {
+        if (!map.has(item.key)) {
+          map.set(item.key, true); // set any value to Map
+          distinctOptions.push({
+            key: item.key,
+            text: item.key,
+            value: item.key,
+          });
+        }
+      }
+      setCompanyOptions(distinctOptions);
     }
   }
+
   useEffect(() => {
     companyRef.current++;
     if (companyRef.current < 4) {
       getCompanyOptions();
     }
   });
+
   return (
     <>
       <Grid>
