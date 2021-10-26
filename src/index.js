1abe889e15fc312db05f97df859a0b74ea9bbc32
Alex Klick <aklick12@gmail.com>
2021-10-25 18:31:40 -0600

    added semantic css

--- src/index.js
+++ src/index.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App/App';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./Components/App/App";
+import { BrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  gql
+  gql,
 } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://salary-sight-be.herokuapp.com/graphql",
-    cache: new InMemoryCache()
+  uri: "https://salary-sight-be.herokuapp.com/graphql",
+  cache: new InMemoryCache(),
 });
 
-export const GET_POST = gql `
+export const GET_POST = gql`
   query {
-      posts {
-        id
+    posts {
+      id
+      username
+      company
+      salary
+      degree
+      positionTitle
+      state
+      jobHuntDuration
+      gender
+      age
+      yearsOfExperience
+      gradYear
+      typeOfEmployment
+      locationOfEmployment
+      negotiation
+      firstPosition
+      program
+    }
+  }
+`;
+export const CREATE_POST = gql`
+  mutation createPost(
+    $username: String
+    $gender: String!
+    $age: String!
+    $gradYear: String!
+    $program: String!
+    $degree: String!
+    $firstPosition: String!
+    $jobHuntDuration: String!
+    $yearsOfExperience: String!
+    $positionTitle: String!
+    $company: String!
+    $locationOfEmployment: String!
+    $typeOfEmployment: String!
+    $state: String!
+    $negotiation: String!
+    $salary: String!
+  ) {
+    createPost(
+      input: {
+        username: $username
+        company: $company
+        salary: $salary
+        degree: $degree
+        positionTitle: $positionTitle
+        state: $state
+        jobHuntDuration: $jobHuntDuration
+        gender: $gender
+        age: $age
+        yearsOfExperience: $yearsOfExperience
+        gradYear: $gradYear
+        program: $program
+        typeOfEmployment: $typeOfEmployment
+        locationOfEmployment: $locationOfEmployment
+        negotiation: $negotiation
+        firstPosition: $firstPosition
+      }
+    ) {
+      post {
         username
         company
         salary
@@ -37,57 +97,14 @@ export const GET_POST = gql `
         program
       }
     }
-  `
-  export const CREATE_POST = gql `
-    mutation createPost($username: String, $gender: String!, $age: String!, $gradYear: String!,
-      $program: String!, $degree: String!, $firstPosition: String!, $jobHuntDuration: String!,
-      $yearsOfExperience: String!, $positionTitle: String!, $company: String!, $locationOfEmployment: String!,
-      $typeOfEmployment: String!, $state: String!, $negotiation: String!, $salary: String!) {
-      createPost (input: {
-        username: $username,
-        company: $company,
-        salary: $salary,
-        degree: $degree,
-        positionTitle: $positionTitle,
-        state: $state,
-        jobHuntDuration: $jobHuntDuration,
-        gender: $gender,
-        age: $age,
-        yearsOfExperience: $yearsOfExperience,
-        gradYear: $gradYear,
-        program: $program,
-        typeOfEmployment: $typeOfEmployment,
-        locationOfEmployment: $locationOfEmployment,
-        negotiation: $negotiation,
-        firstPosition: $firstPosition,
-      }) {
-        post {
-            username
-            company
-            salary
-            degree
-            positionTitle
-            state
-            jobHuntDuration
-            gender
-            age
-            yearsOfExperience
-            gradYear
-            typeOfEmployment
-            locationOfEmployment
-            negotiation
-            firstPosition
-            program
-        }
-      }
-    }
-  `
+  }
+`;
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
