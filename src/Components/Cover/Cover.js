aa9d56b062ad272b8693601bcb568e2c6164de9c
Erica <ericaspitz522@gmail.com>
2021-10-26 14:48:42 -0600

    Add median salary and format salary numbers on cover and cards

--- src/Components/Cover/Cover.js
+++ src/Components/Cover/Cover.js
@@ -4,7 +4,8 @@ const Cover = ({ data }) => {
 
   let formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD'
+    currency: 'USD',
+    minimumFractionDigits: 0
   });
 
   const formatSalary = () => {
@@ -19,7 +20,7 @@ const Cover = ({ data }) => {
      <div className="coverview">
         <div className='text'>
             <h1 className="cover-title">Ensuring Turing graduates get the salary they deserve.</h1>
-            {data && <h2 className="median">{formatSalary()}</h2>}
+            {data && <div className='salary-info'><p className='med-sal'>current median:</p><h2 className="median">{formatSalary()}</h2></div>}
         </div>
      </div>
     </section>
