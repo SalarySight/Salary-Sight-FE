12765b8a2f349bc104fff7803e77ba4fd90e8bee
Erica <ericaspitz522@gmail.com>
2021-10-26 14:31:30 -0600

    Add median salary display on cover component

--- src/Components/Cover/Cover.js
+++ src/Components/Cover/Cover.js
@@ -1,12 +1,25 @@
 import "./Cover.css";
 
-const Cover = () => {
+const Cover = ({ data }) => {
+
+  let formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  const formatSalary = () => {
+    if (data) {
+      return formatter.format(data.salary.medianSalary);
+    }
+  }
+
   return (
     <>
     <section className="cover">
      <div className="coverview">
         <div className='text'>
             <h1 className="cover-title">Ensuring Turing graduates get the salary they deserve.</h1>
+            {data && <h2 className="median">{formatSalary()}</h2>}
         </div>
      </div>
     </section>
