4d63e9f9f0ab8ea381aacf4c5f994e82643476f4
Alex Klick <aklick12@gmail.com>
2021-10-25 20:57:32 -0600

    statistics?

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -17,63 +17,66 @@ const Card = ({
   jobHuntDuration,
 }) => {
   return (
-    <Cardd>
-      <Cardd.Content>
-        <Cardd.Header> {name ? name : "Anonymous"}</Cardd.Header>
-        <Statistic.Group size="tiny" widths="two">
-          <Statistic>
-            <Statistic.Label>salary</Statistic.Label>
-            <Statistic.Value>${salary}</Statistic.Value>
-          </Statistic>
-          <Statistic>
-            <Statistic.Label>job title</Statistic.Label>
-            <Statistic.Value>{positionTitle}</Statistic.Value>
-          </Statistic>
-        </Statistic.Group>
-      </Cardd.Content>
-      <Cardd.Content>
-        <Statistic.Group size="mini" widths="two">
-          <Statistic>
-            <Statistic.Label>company</Statistic.Label>
-            <Statistic.Value>{company}</Statistic.Value>
-          </Statistic>
-          <Statistic>
-            <Statistic.Label>WFH or Commute?</Statistic.Label>
-            <Statistic.Value>{locationOfEmp}</Statistic.Value>
-          </Statistic>
-          <Statistic>
-            <Statistic.Label>term</Statistic.Label>
-            <Statistic.Value>{typeOfEmp}</Statistic.Value>
-          </Statistic>
-          <Statistic>
-            <Statistic.Value>{yearsExp}</Statistic.Value>
-            <Statistic.Label>years of experience</Statistic.Label>
-          </Statistic>
-        </Statistic.Group>
-        <Statistic.Group size="mini" widths="one">
-          <Statistic>
-            <Statistic.Label>Age</Statistic.Label>
-            <Statistic.Value>{age}</Statistic.Value>
-          </Statistic>
-          <Statistic>
-            <Statistic.Label>gender identity</Statistic.Label>
-            <Statistic.Value>{gender}</Statistic.Value>
-          </Statistic>
-          <Statistic>
-            <Statistic.Value>{state}</Statistic.Value>
-            <Statistic.Label>state</Statistic.Label>
-          </Statistic>
-          <Statistic>
-            <Statistic.Value>{jobHuntDuration}</Statistic.Value>
-            <Statistic.Label>time to get a job</Statistic.Label>
-          </Statistic>
-          <Statistic>
-            <Statistic.Value>{negotiate}</Statistic.Value>
-            <Statistic.Label>did they negotiate?</Statistic.Label>
-          </Statistic>
-        </Statistic.Group>
-      </Cardd.Content>
-    </Cardd>
+    <Grid>
+      <Grid.Row></Grid.Row>
+      <Cardd>
+        <Cardd.Content>
+          <Cardd.Header> {name ? name : "Anonymous"}</Cardd.Header>
+          <Statistic.Group size="tiny" widths="two">
+            <Statistic>
+              <Statistic.Label>salary</Statistic.Label>
+              <Statistic.Value>${salary}</Statistic.Value>
+            </Statistic>
+            <Statistic>
+              <Statistic.Label>job title</Statistic.Label>
+              <Statistic.Value>{positionTitle}</Statistic.Value>
+            </Statistic>
+          </Statistic.Group>
+        </Cardd.Content>
+        <Cardd.Content>
+          <Statistic.Group size="mini" widths="two">
+            <Statistic>
+              <Statistic.Label>company</Statistic.Label>
+              <Statistic.Value>{company}</Statistic.Value>
+            </Statistic>
+            <Statistic>
+              <Statistic.Label>WFH or Commute?</Statistic.Label>
+              <Statistic.Value>{locationOfEmp}</Statistic.Value>
+            </Statistic>
+            <Statistic>
+              <Statistic.Label>term</Statistic.Label>
+              <Statistic.Value>{typeOfEmp}</Statistic.Value>
+            </Statistic>
+            <Statistic>
+              <Statistic.Value>{yearsExp}</Statistic.Value>
+              <Statistic.Label>years of experience</Statistic.Label>
+            </Statistic>
+          </Statistic.Group>
+          <Statistic.Group size="mini" widths="one">
+            <Statistic>
+              <Statistic.Label>Age</Statistic.Label>
+              <Statistic.Value>{age}</Statistic.Value>
+            </Statistic>
+            <Statistic>
+              <Statistic.Label>gender identity</Statistic.Label>
+              <Statistic.Value>{gender}</Statistic.Value>
+            </Statistic>
+            <Statistic>
+              <Statistic.Value>{state}</Statistic.Value>
+              <Statistic.Label>state</Statistic.Label>
+            </Statistic>
+            <Statistic>
+              <Statistic.Value>{jobHuntDuration}</Statistic.Value>
+              <Statistic.Label>time to get a job</Statistic.Label>
+            </Statistic>
+            <Statistic>
+              <Statistic.Value>{negotiate}</Statistic.Value>
+              <Statistic.Label>did they negotiate?</Statistic.Label>
+            </Statistic>
+          </Statistic.Group>
+        </Cardd.Content>
+      </Cardd>
+    </Grid>
   );
 };
 
