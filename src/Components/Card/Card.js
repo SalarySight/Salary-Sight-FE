1148708ab504be4d9900ce209b596d8ff399797d
Alex Klick <aklick12@gmail.com>
2021-10-28 16:54:43 -0600

    updated card format

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,5 +1,5 @@
 import "./Card.css";
-import { Card as Cardd, Statistic, Grid } from "semantic-ui-react";
+
 const Card = ({
   name,
   age,
@@ -15,68 +15,71 @@ const Card = ({
   typeOfEmp,
   locationOfEmp,
   jobHuntDuration,
+  program,
+  firstPosition,
 }) => {
+  let formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  });
+
+  const formatSalary = () => {
+    if (salary) {
+      return formatter.format(salary);
+    }
+  };
+
   return (
-    <Grid>
-      <Grid.Row></Grid.Row>
-      <Cardd>
-        <Cardd.Content>
-          <Cardd.Header> {name ? name : "Anonymous"}</Cardd.Header>
-          <Statistic.Group size="tiny" widths="two">
-            <Statistic>
-              <Statistic.Label>salary</Statistic.Label>
-              <Statistic.Value>${salary}</Statistic.Value>
-            </Statistic>
-            <Statistic>
-              <Statistic.Label>job title</Statistic.Label>
-              <Statistic.Value>{positionTitle}</Statistic.Value>
-            </Statistic>
-          </Statistic.Group>
-        </Cardd.Content>
-        <Cardd.Content>
-          <Statistic.Group size="mini" widths="two">
-            <Statistic>
-              <Statistic.Label>company</Statistic.Label>
-              <Statistic.Value>{company}</Statistic.Value>
-            </Statistic>
-            <Statistic>
-              <Statistic.Label>WFH or Commute?</Statistic.Label>
-              <Statistic.Value>{locationOfEmp}</Statistic.Value>
-            </Statistic>
-            <Statistic>
-              <Statistic.Label>term</Statistic.Label>
-              <Statistic.Value>{typeOfEmp}</Statistic.Value>
-            </Statistic>
-            <Statistic>
-              <Statistic.Value>{yearsExp}</Statistic.Value>
-              <Statistic.Label>years of experience</Statistic.Label>
-            </Statistic>
-          </Statistic.Group>
-          <Statistic.Group size="mini" widths="one">
-            <Statistic>
-              <Statistic.Label>Age</Statistic.Label>
-              <Statistic.Value>{age}</Statistic.Value>
-            </Statistic>
-            <Statistic>
-              <Statistic.Label>gender identity</Statistic.Label>
-              <Statistic.Value>{gender}</Statistic.Value>
-            </Statistic>
-            <Statistic>
-              <Statistic.Value>{state}</Statistic.Value>
-              <Statistic.Label>state</Statistic.Label>
-            </Statistic>
-            <Statistic>
-              <Statistic.Value>{jobHuntDuration}</Statistic.Value>
-              <Statistic.Label>time to get a job</Statistic.Label>
-            </Statistic>
-            <Statistic>
-              <Statistic.Value>{negotiate}</Statistic.Value>
-              <Statistic.Label>did they negotiate?</Statistic.Label>
-            </Statistic>
-          </Statistic.Group>
-        </Cardd.Content>
-      </Cardd>
-    </Grid>
+    <article className="card">
+      {name ? (
+        <p className="name card-info">
+          <b>Name:</b> {name}
+        </p>
+      ) : (
+        <p className="name card-info">
+          <b>Name:</b> Anonymous
+        </p>
+      )}
+      <p className="gender card-info">
+        <b>Gender Identity:</b>
+        <br />
+        {gender}
+      </p>
+      <p className="program card-info">
+        <b>Program:</b> {program}
+      </p>
+      <p className="age card-info">
+        <b>Age:</b> {age}
+      </p>
+      <p className="salary-num card-info">
+        <b>Salary:</b> {formatSalary()}
+      </p>
+      {company ? (
+        <p className="company card-info">
+          <b>Company:</b> {company}
+        </p>
+      ) : (
+        <p className="company card-info">
+          <b>Company:</b> Not Specifed
+        </p>
+      )}
+      <p className="years-exp card-info">
+        <b>Years of Experience:</b> {yearsExp}
+      </p>
+      <p className="position-title card-info">
+        <b>Job Title:</b> {positionTitle}
+      </p>
+      <p className="type-of-emp card-info">
+        <b>Type of Employment:</b> {typeOfEmp}
+      </p>
+      <p className="state card-info">
+        <b>State:</b> {state}
+      </p>
+      <p className="job-hunt-duration card-info">
+        <b>Job Hunt Duration:</b> {jobHuntDuration} days
+      </p>
+    </article>
   );
 };
 
